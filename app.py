4e93c4358c6038f1f4a2ccb3acf991bfa7786de4
Imanmh369 <hussainiman48@gmail.com>
2026-06-27 11:18:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
 import dash
+import dash_bootstrap_components as dbc
 from dash import dcc, html, Input, Output
 import encyclopedia, demographic
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
+# Create the app with Bootstrap theme
+app = dash.Dash(
+    __name__,
+    suppress_callback_exceptions=True,
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    pages_folder=""  # disable automatic pages folder requirement
+)
+
 server = app.server
 
-app.layout = html.Div([
-    html.H1("Welcome to the Art Encyclopedia"),
-    dcc.Location(id="url", refresh=False),
-    html.Div([
-        dcc.Link("Encyclopedia Timeline", href="/encyclopedia"),
-        html.Br(),
-        dcc.Link("Demographic", href="/demographic")
-    ]),
-    html.Div(id="page-content")
-])
+# Navbar
+navbar = dbc.NavbarSimple(
+    brand="Art Encyclopedia",
+    brand_href="/",
+    color="dark",
+    dark=True,
+    children=[
+        dbc.NavItem(dbc.NavLink("Encyclopedia Timeline", href="/encyclopedia")),
+        dbc.NavItem(dbc.NavLink("Demographic", href="/demographic")),
+    ]
+)
 
+# Layout
+app.layout = dbc.Container([
+    navbar,
+    dcc.Location(id="url", refresh=False),
+    html.Div(id="page-content", className="mt-4")
+], fluid=True)
+
+# Page routing
 @app.callback(Output("page-content", "children"), Input("url", "pathname"))
 def display_page(pathname):
     if pathname == "/encyclopedia":
@@ -34,3 +51,4 @@ demographic.register_callbacks(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
